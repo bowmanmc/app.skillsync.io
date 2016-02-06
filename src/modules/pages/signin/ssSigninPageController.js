ccb89ef65a5b229d2301fe0d9532355a3c97ec7a
Michael Bowman <bowmanmc@gmail.com>
2016-02-05 21:17:54 -0500

    signin page

--- src/modules/pages/signin/ssSigninPageController.js
+++ src/modules/pages/signin/ssSigninPageController.js
@@ -2,10 +2,35 @@
 
 module.exports = function(ngModule) {
 
-    ngModule.controller('ssSigninPageController', function() {
+    ngModule.controller('ssSigninPageController', function($scope) {
+
+        $scope.checkAuth = function() {
+            var email = $scope.input.email;
+            var password = $scope.input.password;
+
+            // Some quick and dirty form validation... see how to do this
+            // better with ember...
+            $scope.errors = [];
+            if (!email || email.indexOf('@') < 0 || email.indexOf('.') < 0) {
+                $scope.errors.push('Please enter your email address.');
+            }
+            if (!password) {
+                $scope.errors.push('Please enter your password.');
+            }
+
+            // no need to bother our server if we have errors to fix
+            if ($scope.errors.length > 0) {
+                return;
+            }
+
+            // Let's check the email/password at the server and get a token
+            console.log('Signing in with ' + email + '/"' + password + '"...');
+        };
 
         var initialize = function() {
             console.log('ssSigninPageController initializing...');
+            $scope.errors = [];
+            $scope.input = {};
         };
         initialize();
     });
