3df0280c8720c30331d1cfc35aa251a078ebc761
Michael Bowman <bowmanmc@gmail.com>
2016-02-06 20:28:18 -0500

    stubs for all of the account screens

--- src/modules/routing/index.js
+++ src/modules/routing/index.js
@@ -11,18 +11,29 @@ module.exports = function(ngModule) {
 
     ngModule.config(function($routeProvider) {
         $routeProvider
+        // Main Dashboard
         .when('/', {
              controller: 'ssMainPageController',
             templateUrl: 'modules/pages/main/ssMainPageTemplate.html'
         })
-        .when('/account/signin', {
-             controller: 'ssAccountSigninPageController',
-            templateUrl: 'modules/pages/account/ssAccountSigninPageTemplate.html'
+        // Account Pages
+        .when('/account/edit', {
+             controller: 'ssAccountEditPageController',
+            templateUrl: 'modules/pages/account/ssAccountEditPageTemplate.html'
         })
         .when('/account/register', {
              controller: 'ssAccountRegisterPageController',
             templateUrl: 'modules/pages/account/ssAccountRegisterPageTemplate.html'
         })
+        .when('/account/reset', {
+             controller: 'ssAccountResetPageController',
+            templateUrl: 'modules/pages/account/ssAccountResetPageTemplate.html'
+        })
+        .when('/account/signin', {
+             controller: 'ssAccountSigninPageController',
+            templateUrl: 'modules/pages/account/ssAccountSigninPageTemplate.html'
+        })
+
         .otherwise({
             redirectTo: '/'
         });
