c8983b4d0f5b2424b2edf29bc864d83215fee477
Michael Bowman <bowmanmc@gmail.com>
2016-02-15 14:30:57 -0500

    education form

--- src/modules/pages/resume/ssResumeWorkPageController.js
+++ src/modules/pages/resume/ssResumeWorkPageController.js
@@ -2,10 +2,27 @@
 
 module.exports = function(ngModule) {
 
-    ngModule.controller('ssResumeWorkPageController', function() {
+    ngModule.controller('ssResumeWorkPageController', function($scope) {
 
         var initialize = function() {
             console.log('ssResumeWorkPageController initializing...');
+
+            $scope.work = [{
+                name: 'Applied Information Sciences',
+                url:  'http://appliedis.com',
+                startDate: '2014-01',
+                endDate: null,
+                summary: 'Technical Architect - ',
+                skillsUsed: 'D3.js, AngularJS, Web Mapping'
+            }, {
+                name: 'Booz Allen Hamilton',
+                url: 'http://bah.com',
+                startDate: '2011-03',
+                endDate: '2014-01',
+                summary: 'Software Engineer',
+                skillsUsed: 'Python, jQuery, Django'
+            }];
+
         };
         initialize();
     });
